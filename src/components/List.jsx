9a2b13ab1b403d0ca537a9b243b49aec5319c0a6
Avijit karmaker <ajkacca@gmail.com>
2023-08-15 13:17:25 +0300

    update accordion

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,22 +1,44 @@
 import { data as Info } from "../data";
+import { BsPlusLg } from "react-icons/bs";
+import { AiOutlineClose } from "react-icons/ai";
+import { useState } from "react";
 
 
 const List = () => {
+    const [selectedId, setSelectedId] = useState(null);
+
+    console.log(selectedId);
+
+
+    const toggleDisplay = (id) => {
+        if (selectedId === id) {
+            setSelectedId(null);
+        } else {
+            setSelectedId(id);
+        }
+    };
+
+
     return (
         <div className="bg-white max-w-5xl mx-auto p-4">
             <h1 className="text-3xl uppercase underline text-left min-w-[400px]">Accordion-self closing</h1>
             {Info.map((item, index) => {
                 const { id, title, tagline, description } = item;
+                const isActive = selectedId === id;
 
                 return (
-                    <div className="accordion text-left min-w-[400px] my-4" key={id}>
-                        <div className="header">
+                    <div className="accordion text-left min-w-[800px] my-4" key={id}>
+                        <div className="header flex items-center justify-between my-4 border-b-2 border-gray-200 py-4">
                             <h3 className="text-2xl">{title}</h3>
+                            <button onClick={() => { toggleDisplay(id); }}>
+                                {isActive ? <AiOutlineClose className="text-2xl" /> : <BsPlusLg className="text-2xl" />
+                                }
+                            </button>
                         </div>
-                        <div className="content text-left">
+                        {isActive && <div className="content text-left">
                             <p className="font-semibold">{tagline}</p>
                             <p>{description}</p>
-                        </div>
+                        </div>}
                     </div>
                 );
             })}
